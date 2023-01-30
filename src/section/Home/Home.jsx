0ef064c73f9ffc3df5660b980345c4510d9c2771
Saul huarcaya <shuarcaya99@gmail.com>
2023-01-30 03:07:45 -0500

    home-modificado

--- src/section/Home/Home.jsx
+++ src/section/Home/Home.jsx
@@ -3,16 +3,12 @@ import img_author from "../../assets/Imagenes/001212518.jpg";
 import BotonCV from "../../components/BotonCv/BotonCv";
 import CV from "../../assets/CV/CV-SAUL-HUARCAYA-QUISPE.pdf";
 
-import 'aos/dist/aos.css'; 
-
 function Home(){
 
     return <section id="home">
         <div className="container container-home">
             <div className="container-presentation">
-                
-                <h1 className="text_title" >Hola,Soy Saul Huarcaya</h1>
-
+                <h1 className="text_title">Hola,Soy Saul Huarcaya</h1>
                 <p className="text_description">Manejo tecnologias de frontend y backend me considero entusiasta y perserverante en lo que Hago.</p>
                 <BotonCV text="Descargar CV" tipo="desktop" href={CV}/>
             </div>
